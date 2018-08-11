b0e79b2e88d21b060e6f604f43f84e62d49d4e09
Radosław Bieńczyk <radoslaw.bienczyk@gmail.com>
2018-08-11 11:27:27 +0200

    walczymy dalej

---
+++ jquery/js/doc-ready.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(function () {
+
+	//alert("test");
+
+	$("#first").css("color", "blue").hide(2000).show(3000);
+
+});
---
+++ jquery/js/selector.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//let paragraphs = $("p");
+//console.log(paragraphs);
+
+let parOne = $("#paragraf");
+console.log(parOne);
+
+let parNext = $(".paragrafnext");
+console.log(parNext);
